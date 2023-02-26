4c35e0e4a5bdc3cb5293fdaad90cd4815fe1f773
Kamil Synowiec <1935002@brunel.ac.uk>
2023-02-26 23:49:44 +0100

    Footer added

---
+++ root/frontend/src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import './styles.css';
+
+function Footer(){
+    return(
+        <div id="footer">
+            <div id="new-line">
+
+            </div>
+            <div id="frame">
+                <a class="footer-elements" href="#Privacy">Privacy Policy</a>
+                <a class="footer-elements" href="#Terms">Terms of use</a>
+                <a class="footer-elements" href="#Tech">Technical support</a>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
--- root/frontend/src/pages/Signup/index.js
+++ root/frontend/src/pages/Signup/index.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import './styles.css'; //styles
+import Footer from "../../components/Footer/Footer.js";
 
 function Register(){
     const [registerUsername, setRegisterUsername]= useState("");
@@ -8,19 +9,22 @@ function Register(){
 
     return(
         <div>
-            <h1>Scouts</h1>
-            <div id="register-form">
-                <div id="upper">
-                    <input id="username" placeholder="username" onChange={e => setRegisterUsername(e.target.value)}/>
-                </div>
-                <div id="lower">
-                    <input id="password" placeholder="password" onChange={e => setRegisterPassword(e.target.value)}/>
-                </div>
+            <div>
+                <h1>Scouts</h1>
+                <div id="register-form">
+                    <div id="upper">
+                        <input id="username" placeholder="username" onChange={e => setRegisterUsername(e.target.value)}/>
+                    </div>
+                    <div id="lower">
+                        <input id="password" placeholder="password" onChange={e => setRegisterPassword(e.target.value)}/>
+                    </div>
 
-                <div>
-                    <button>Register</button>
+                    <div>
+                        <button>Register</button>
+                    </div>
                 </div>
             </div>
+            <Footer/>
         </div>
     );
 }

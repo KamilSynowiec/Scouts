f975d38536a8c5a9824d12dcf750dcad0eb50f2c
Kamil Synowiec <1935002@brunel.ac.uk>
2023-02-28 00:00:33 +0100

    routes added

--- root/backend/server.js
+++ root/backend/server.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
-const express = require("express");
+const express = require("express");  //framework used to create http web sever
 const cors= require("cors");
 const passport= require("passport");
 const passportlocal = require("passport-local").Strategy;
-const cookieParser = require("cookie-parser");
+const cookieParser = require("cookie-parser");   //used to parse header to store data on the browser whenever session is established
 const bcrypt = require("bcryptjs");
-const expressSession = require("express-session");
+const expressSession = require("express-session");   //express session creates new memorystore instance to store  session data in a server
 const bodyParser = require("body-parser");
 
 const app = express();
@@ -14,14 +14,45 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(cors({
+    origin: "http://localhost:3000",  //specify which origin can make requests to the server  (location of the react app)
+    credentials: true
+}));
+
 app.use(expressSession({
-    secret: "secretcode",
+    secret: "secretcode",   //used to authenticate session
     resave: true,
     saveUninitialized: true
 }));
 
-
 //whenever you call the secret inside session you need to use cookieParser
+app.use(cookieParser("secretcode"));
+
+//serving public file
+app.use(express.static("../frontend"));
+
+
+
+//Routes
+app.post("/login", (req, res)=>{
+    console.log(req.body);
+});
+
+
+app.post("/login", (req, res)=>{
+    console.log(req.body);
+});
+
+app.post("/register", (req, res)=>{
+    console.log(req.body);
+});
+
+app.post("/user", (req,res)=>{
+    console.log(req.body);
+});
+
+
+
 
 app.listen(4000, ()=> {
     console.log("Server up and running...");

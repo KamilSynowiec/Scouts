b87fdde3dd6c9e55aef88f923ca798f787ea98d0
Kamil Synowiec <1935002@brunel.ac.uk>
2023-02-27 01:27:09 +0100

    basic server setup

--- root/backend/server.js
+++ root/backend/server.js
@@ -4,8 +4,16 @@ const cors= require("cors");
 const passport= require("passport");
 const passportlocal = require("passport-local").Strategy;
 const cookieParser = require("cookie-parser");
-const bcrypt = require("bcyptjs");
+const bcrypt = require("bcryptjs");
 const expressSession = require("express-session");
 const bodyParser = require("body-parser");
 
 const app = express();
+
+//Middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.listen(4000, ()=> {
+    console.log("Server up and running...");
+});

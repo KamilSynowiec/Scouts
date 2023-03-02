f9da3dc479d6c973562537e8fc35184cb180f115
Kamil Synowiec <1935002@brunel.ac.uk>
2023-03-03 00:50:37 +0100

    passportConfig

---
+++ root/backend/passportConfig.js
@@ -0,0 +1,32 @@
+const User = require("./user.js");
+const bcrypt = require("bcryptjs");
+const localStrategy = require("passport-local").Strategy; //local strategy - authenticaiton with username and password
+
+module.exports = function(passport){
+
+    passport.use(
+        new localStrategy((username, password, done)=>{
+            User.findOne({username: username}, (error,user)=>{
+                if(error){
+                    throw error;
+                }
+                if(!user){
+                    return done(null, false); //null means no error and false means no user was found
+
+                }else{  //if there is user
+                    bcrypt.compare(password, user.password, (error, result)=>{
+                        if(error){
+                            throw error;  
+                        }
+                        if(result===true){
+                            return done(null, user);  //if the password is correct return user
+                        }else{
+                            return done(null, false);  //if the password is not correct return false
+                        }
+                    });
+                }
+
+            });
+        })
+    );
+}

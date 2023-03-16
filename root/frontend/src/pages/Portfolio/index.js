a0e24783fa10fdcc482efea5fd1cbb89cdf02257
Kamil Synowiec <1935002@brunel.ac.uk>
2023-03-16 17:17:24 +0100

    add and delete into portfolio

--- root/frontend/src/pages/Portfolio/index.js
+++ root/frontend/src/pages/Portfolio/index.js
@@ -1,15 +1,18 @@
 import Footer from "../../components/Footer/Footer.js";
 import Navigation from "../../components/Navigation/index.js";
 import React from "react";
+import {v4 as uuidv4} from 'uuid';
+
+import './portfolio_styles.css';
 
 const initialList=[
     {
-        id: 'a',
         name: 'Robin',
+        id: 'a',
     },
     {
-        id: 'b',
         name: 'Dennis',
+        id: 'b',
     },
 ];
 
@@ -18,38 +21,61 @@ function Portfolio(){
     const [list, setList]=React.useState(initialList);
     const [name, setName]=React.useState('');
 
-    let achievements = ["achievement_1", "achievement_2", "achievement_3", "achievement_4"];
-
     //let userObj=JSON.parse(document.cookie);
 
-    function handleChange(){ //handle input field's state
+    function handleChange(event){ //handle input field's state/ saving input's value into name variable
+        setName(event.target.value);
+
 
     }
 
-    function handleAdd(){ //add item
-        
+    function handleAdd(){ //add item to current list and update
+        const newList = list.concat({name, id: uuidv4()});
+
+        setList(newList);
+
+        setName(''); //clearing input field
     }
     
+    function handleRemove(id){
+        const newList=list.filter((item)=>item.id !== id);
+        
+        setList(newList);
+        
+    }
 
     return(
         <div>
             <Navigation/>
 
-            <div>
+            <div class="center">
                 <input type="text" value={name} onChange={handleChange}/>
                 <button type="button" onClick={handleAdd}>
                     Add
                 </button>
             </div>
 
-            <div>
+            <div class="center">
                 <h2>Your achievements</h2>
-                <ul> 
+                <ul id="portfolio_list"> 
                     {list.map((item)=>(
-                        <li key={item.id}>{item.name}</li>
+                        <div>
+                            {/*achiement element li */}
+                            <li class="portfolio_list_el" key={item.id}>
+                                <span>{item.name}</span>
+                            </li>
+
+                             {/*button to delete achievement */}
+                             <span id="deleteBtn" type="button" onClick={()=> handleRemove(item.id)}>Delete</span>
+                        </div>
+                        
                     ))}
                 </ul>
 
+            </div>
+
+            <div id="right">
+                <span id="addAchvBtn">Add new Achievement</span>
             </div>
 
             
